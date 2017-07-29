864ad643a21b20faf642e9da6db4bb15eac33f23
Mike Lynch <mike.lynch@iinet.net.au>
2017-07-29 17:41:30 +1000

    Finished off a few pending tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -20,6 +20,7 @@ import TextGen (
 import System.Random
 
 import Data.List (intercalate)
+import Control.Monad (forM)
 
 type TextGenCh = TextGen StdGen [[Char]]
 
@@ -46,18 +47,15 @@ main = hspec $ do
   describe "chooseN" $ do
     it "checks (chooseN n ws) returns n elements from ws" $ property $ prop_chooseN
 
+  describe "perhaps" $ do
+    it "checks that perhaps works" $ property $ prop_perhaps
 
   describe "rep" $ do
-    it "checks that rep creates N copies" $ do
-      pending
+    it "checks that rep creates N copies" $ property $ prop_rep
 
   describe "randrep" $ do
-    it "checks that randrep creates (N, M) copies" $ do
-      pending
+    it "checks that randrep creates (N, M) copies" $ property $ prop_randrep
 
-  describe "perhaps" $ do
-    it "checks that perhaps works" $ do
-      pending
 
   
 prop_literals :: [ Char ] -> Property
@@ -99,6 +97,9 @@ prop_list ws = monadicIO $ do
     g1 <- getStdRandom $ runTextGen g
     return $ dumbjoin g1
   assert ( ws' == dumbjoin ws )
+
+
+  
 
 -- choose is unsafe and can't handle empty option lists
 
@@ -156,3 +157,40 @@ genlist ws = do
     return $ dumbjoin g
   return $ "[" ++ (intercalate ", " results) ++ "]"
 
+nperhaps :: Int
+nperhaps = 50000
+
+tolerance :: Float
+tolerance = 0.2
+
+prop_perhaps :: Positive Int -> Positive Int -> [ Char ] -> Property
+prop_perhaps (Positive n0) (Positive m0) w = monadicIO $ do
+  ( n, m ) <- return $ if ( n0 < m0 ) then ( n0, m0 ) else ( m0, n0 )
+  results <- run $ do
+    g <- return $ perhaps ( n, m ) (word w)
+    forM [1..nperhaps] $ \_ -> do
+      getStdRandom $ runTextGen g
+  empties <- return $ length $ filter null results
+  ratio <- return $ (fromIntegral n / fromIntegral m)
+  got <- return $ ((fromIntegral (nperhaps - empties)) / fromIntegral nperhaps)
+  assert ( (abs (got - ratio)) < tolerance )
+
+prop_rep :: Positive Int -> [ Char ] -> Property
+prop_rep (Positive n) w = monadicIO $ do
+  repeats <- run $ do
+    g <- return $ rep n (word w)
+    getStdRandom $ runTextGen g
+  expected <- return $ intercalate " " ( take n $ repeat w )
+  assert $ expected == (dumbjoin repeats)
+
+
+prop_randrep :: Positive Int -> Positive Int -> [ Char ] -> Property
+prop_randrep (Positive a0) (Positive b0) w = monadicIO $ do
+  ( a, b ) <- return $ if a0 < b0 then ( a0, b0 ) else ( b0, a0 )
+  repeats <- run $ do
+    g <- return $ randrep ( a, b ) (word w)
+    getStdRandom $ runTextGen g
+  l <- return $ length repeats
+  expected <- return $ intercalate " " ( take l $ repeat w )
+  assert $ expected == (dumbjoin repeats)
+  assert ( ( l >= a ) && ( l <= b ) )
